417ea2cd9fbf47445eb11190560ae889d0c1b08a
Lorenalgm <lorena@proesc.com>
2019-11-23 23:31:01 -0300

    Importado imagens com react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import profile from './assets/profile.jpeg';
 function App() {
-	return <h1>Olá, Lorena :) </h1>
+	// return <h1>Olá, Lorena :) </h1>
+	
+	return <img width="200px" src={profile} />
+	
+
 }
 
 export default App;
